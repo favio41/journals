1873d355c1c9c865320533550697f27465fbb3dd
Susana <favio41>
2020-09-20 07:19:16 +0100

    deploy

---
+++ precache-manifest.341c2c938b05d6b43848fc1553a98141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c53a957f3ea7d996cd55c24742f668f",
+    "url": "/journals/index.html"
+  },
+  {
+    "revision": "1f06a98abecd35e6fc49",
+    "url": "/journals/static/css/main.7160b256.chunk.css"
+  },
+  {
+    "revision": "03c862c6d1169dbf3069",
+    "url": "/journals/static/js/2.e5fce223.chunk.js"
+  },
+  {
+    "revision": "7a29fd67914f99c562dc3f65bbf1078b",
+    "url": "/journals/static/js/2.e5fce223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f06a98abecd35e6fc49",
+    "url": "/journals/static/js/main.a9ceb1d9.chunk.js"
+  },
+  {
+    "revision": "2f531817f08eaab58367",
+    "url": "/journals/static/js/runtime-main.a51313b3.js"
+  }
+]);
